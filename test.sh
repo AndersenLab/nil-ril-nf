ad0826d196ec2e7c81d67ce0afea02f3e07ea9cb
Daniel Cook <danielecook@gmail.com>
2017-10-08 17:11:59 -0500

    Fix paths

--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-script_path="../main.nf"
-data_path="../test_data"
-
 curl --version >/dev/null 2>&1 || { echo >&2 "I require curl, but it's not installed. Aborting."; exit 1; }
 tar --version >/dev/null 2>&1 || { echo >&2 "I require tar, but it's not installed. Aborting."; exit 1; }
 docker -v >/dev/null 2>&1 || { echo >&2 "I require docker, but it's not installed. Visit https://www.docker.com/products/overview#/install_the_platform  ."; exit 1; }
@@ -15,12 +12,12 @@ curl andersen/genome/c_elegans/WS245 > WS245.tar.gz
 tar -xvzf WS245.tar.gz
 
 
-cmd="""nextflow run ../main.nf -resume \
+cmd="""nextflow run main.nf -resume \
                                -name \"$run_name\" \
                                --reference WS245.fa.gz
-                               --fqs ../test_data/fq_sheet.tsv \
+                               --fqs /test_data/fq_sheet.tsv \
                                --with-docker andersenlab/nil-ril \
-                               --VCF ../test_data/N2_CB.simple.vcf.gz \
+                               --VCF /test_data/N2_CB.simple.vcf.gz \
                                --cores 1 """
 
 echo "Starting nextflow... Command:"
